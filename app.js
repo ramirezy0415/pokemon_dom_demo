87a8c843b98833dfa9c4305ef43030a8b55ca89d
Charles Lucas <60566705+Clucas0311@users.noreply.github.com>
2025-08-14 18:11:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+ * @typedef Pokemon
+ * @property {string} name
+ * @property {string} type
+ * @property {number} hp
+ * @property {string[]} moves
+ */
+
+// === Constants ===
+const POKEMON_NAMES = ["Charmander", "Squirtle", "Bulbasaur"];
+const POKEMON_TYPES = ["Fire", "Water", "Grass"];
+const POKEMON_HPS = [39, 44, 45];
+const POKEMON_MOVES = [
+  ["Scratch", "Ember"],
+  ["Tackle", "Water Gun"],
+  ["Tackle", "Vine Whip"],
+];
+
+/** @returns {Pokemon} a Pokémon from constants by index */
+
+/** @returns {number} the average HP of all `pokemons` */
